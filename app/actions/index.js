57997ad928bb55454643a8aa8b8b82c35f481dd2
Sharif-Miah <farhansharif1410@gmail.com>
2025-02-25 20:50:10 +0600

    send email to payment method

--- app/actions/index.js
+++ app/actions/index.js
@@ -1,13 +1,16 @@
 'use server';
 
+import EmailTemplate from '@/components/payments/EmailTemplate';
 import {
   createUser,
   findUserByCredentials,
+  getEventById,
   updateGoing,
   updateInterest,
 } from '@/db/queries';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import { Resend } from 'resend';
 
 async function registerUser(formData) {
   const user = Object.fromEntries(formData);
@@ -47,4 +50,27 @@ async function addGoingEvent(eventId, user) {
   redirect('/');
 }
 
-export { registerUser, performLogin, addInterestedEvent, addGoingEvent };
+async function sendEmail(eventId, user) {
+  try {
+    const event = await getEventById(eventId);
+    const resend = new Resend(process.env.RESEND_API_KEY);
+    const message = `Dear ${user?.name}, you have been successfully registered for the event, ${event.name}. Please carry this email and your official id to the venue. We are excited to have you here.`;
+
+    const sent = await resend.emails.sent({
+      from: 'noreply@noreply.io',
+      to: user?.email,
+      subject: 'Successfully Registered for the event.',
+      react: EmailTemplate({ message }),
+    });
+  } catch (error) {
+    throw error;
+  }
+}
+
+export {
+  registerUser,
+  performLogin,
+  addInterestedEvent,
+  addGoingEvent,
+  sendEmail,
+};
